2072f2b8a895452498309288981c3bd4adb2d965
Vitor Katakura <7vitorgomez@gmail.com>
2025-02-16 17:43:42 -0300

    Exercicio finalizado

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -9,31 +9,30 @@ void btn_callback(void) {
 }
 
 void main(void) {
-    int g_cnt = 0;
-    char g_str[10];
-    int batata;
-
-    if (pressed) {
-        printf("btn pressed \n");
-
-        if (g_cnt >= 8)
-            g_cnt = 0;
-
-        g_cnt = 2;
-
-        int i = 0;
-        for (i = 0; i < g_cnt; i++) {
-            g_str = 1;
-            g_str[i] = '*';
-            g_str[i + 1] = NULLL;
-            delay_ms(50);
-            gfx_mono_draw_string(g_str, 0, 0, &sysfont);
-        }
-
-        pressed = 0;
-    }
-
     batata = batata + 1;
     while (1) {
+        int g_cnt = 0;
+        char g_str[10];
+        int batata;
+
+        if (pressed) {
+            printf("btn pressed \n");
+
+            if (g_cnt >= 8)
+                g_cnt = 0;
+
+            g_cnt = 2;
+
+            int i = 0;
+            for (i = 0; i < g_cnt; i++) {
+                g_str = 1;
+                g_str[i] = '*';
+                g_str[i + 1] = NULLL;
+                delay_ms(50);
+                gfx_mono_draw_string(g_str, 0, 0, &sysfont);
+            }
+
+            pressed = 0;
+        }
     }
 }
--- main_isr_practice.c
+++ main_isr_practice.c
@@ -4,49 +4,38 @@
 
 const int BTN_PIN_R = 28;
 
-// ERRO: cppcheck unitvar
-// ERRO: addon IsrVarVolatile
-int btn_flag;
+int volatile btn_flag = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
-
-    // ERRO: addon IsrPrintf
-    printf("btn pressed \n");
-
-    // ERRO: addon IsrNoLoop
-    while (!pio_get(BTN_PIN_R)) {
-      // ERRO: addon IsrNoDelay
-      sleep_ms(1);
+    if (events == 0x4) { // fall edge
+        btn_flag = 1;
     }
-
-    // ERRO: addon IsrPrintf
-    printf("btn released \n");
-
-    // ERRO: addon IsrNoDelay
-    sleep_ms(1);
-    btn_flag = 1;
-  }
 }
 
 int main() {
-  stdio_init_all();
-  gpio_init(BTN_PIN_R);
-  gpio_set_dir(BTN_PIN_R, GPIO_IN);
-  gpio_pull_up(BTN_PIN_R);
-  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                     &btn_callback);
+    stdio_init_all();
+    gpio_init(BTN_PIN_R);
+    gpio_set_dir(BTN_PIN_R, GPIO_IN);
+    gpio_pull_up(BTN_PIN_R);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
+                                       &btn_callback);
 
-  volatile int capture_flag = 0;
-  int a;
-  while (1) {
-    if (btn_flag) {
-      capture_flag = 1;
-      btn_flag = 0;
-    }
+    int capture_flag = 0;
+    int a;
+    while (1) {
+        if (btn_flag) {
+            printf("btn pressed \n");
 
-    if (capture_flag) {
+            while (!pio_get(BTN_PIN_R)) {
+                sleep_ms(1);
+            }
+
+            capture_flag = 1;
+            btn_flag = 0;
+        }
+
+        if (capture_flag) {
+            printf("btn released \n");
+        }
     }
-    
-  }
 }
